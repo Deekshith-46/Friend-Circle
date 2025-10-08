36b15d04abfcf029673d92dba0daac9bac5bcf1c
Deekshith-46 <tdeekshith46@gmail.com>
2025-10-08 12:13:16 +0530

    New Version

--- src/server.js
+++ src/server.js
@@ -1,6 +1,11 @@
 const app = require('./app');
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+// For Vercel deployment
+if (process.env.NODE_ENV === 'production') {
+  module.exports = app;
+} else {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
